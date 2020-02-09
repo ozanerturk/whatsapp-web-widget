e524807be1eda629de5fd7ccf39a1957ac2624d7
Ozan ERTÜRK <ozanerturk@github.com>
2020-02-09 19:04:43 +0300

    removed unneccessary types

--- server/app.js
+++ server/app.js
@@ -8,38 +8,6 @@ app.use(function (req, res, next) {
     next();
 })
 
-var database =
-{
-    "http://ozan-s540:1234": {
-        agents: [
-            {
-                name: "Agent0",
-                phone: "+9005399413336",
-                photo: "https://randomuser.me/api/portraits/men/18.jpg"
-            },
-            {
-                name: "Agent1",
-                phone: "+9005329413336",
-                photo: "https://randomuser.me/api/portraits/men/19.jpg"
-            }
-        ]
-    },
-    "http://ozanerturk.com:1234": {
-        agents: [
-            {
-                name: "Agent2",
-                phone: "+9425399413336",
-                photo: "https://randomuser.me/api/portraits/men/20.jpg"
-            },
-            {
-                name: "Agent3",
-                phone: "+902329413336",
-                photo: "https://randomuser.me/api/portraits/men/21.jpg"
-            }
-        ]
-    }
-}
-
 app.get('/cred', (req, res) => {
     var randomuApiUrl = "https://randomuser.me/api/?results=4&seed=" + req.headers["origin"];
     fetch(randomuApiUrl).then(response=>{
